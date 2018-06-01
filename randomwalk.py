36b058bb8889bd11f9e896114cf93249ee5bd078
liupu14 <lpgauss@163.com>
2018-06-01 13:06:58 +0800

    add main contents

--- randomwalk.py
+++ randomwalk.py
@@ -1,3 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
+rands = np.random.randint(0,1,1000)
+walk = np.where(rands > 0,1,-1)
+walks = walk.cumsum()
+plt.plot(walks)
+plt.title('Random Walk')
+plt.xlabel('Step')
+plt.ylabel('position')
+
